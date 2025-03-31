93681bd4927ab997bb3e16a28f2eb4480f1d83c7
Matthew Ryan Granger <mattg1827@gmail.com>
2025-03-31 13:05:23 -0700

    @ f InputDispatcher class has a comment code block to address, class coded out for functionality

---
+++ InputDispatcher.cpp
@@ -0,0 +1,26 @@
+#include "InputDispatcher.h"
+
+InputDispatcher::InputDispatcher(sf::RenderWindow* window)
+{
+  m_Window = window;
+}
+
+void InputDispatcher::dispatchInputEvents()
+{
+  sf::Event event;
+  while (m_Window->pollEvent(event))
+  {
+    // if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+    // m_Window->close();}
+  }
+  for (const auto& ir: m_InputReceivers)
+  {
+    ir->addEvent(event);
+  }
+}
+
+void InputDispatcher::registerNewInputReceiver(InputReceiver* ir)
+{
+  m_InputReceivers.push_back(ir);
+}
+
--- InputDispatcher.h
+++ InputDispatcher.h
@@ -5,7 +5,10 @@
 class InputDispatcher
 {
 private:
-
+  sf::RenderWindow* m_Window;
+  vector<InputReceiver*> m_InputReceivers;
 public:
-
+  InputDispatcher(sf::RenderWindow* window);
+  void dispatchInputEvents();
+  void registerNewInputReceiver(InputReceiver* ir);
 };
