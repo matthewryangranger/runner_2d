81e30d3759fa08914d1585c3d5bd107ee7c6417f
Matthew Ryan Granger <mattg1827@gmail.com>
2025-04-27 16:29:21 -0700

    @ f PlayerUpdate class coded

--- PlayerUpdate.cpp
+++ PlayerUpdate.cpp
@@ -1,5 +1,36 @@
-//
-// Created by Matthew Granger on 4/27/25.
-//
-
 #include "PlayerUpdate.h"
+#include "SoundEngine.h"
+#include "LevelUpdate.h"
+
+sf::FloatRect* PlayerUpdate::getPositionPointer()
+{
+  return &m_Position;
+}
+
+bool* PlayerUpdate::getGroundedPointer()
+{
+  return &m_isGrounded;
+}
+
+InputReceiver* PlayerUpdate::getInputReceiver()
+{
+  return &m_InputReceiver;
+}
+
+void PlayerUpdate::assemble(std::shared_ptr<LevelUpdate> levelUpdate, std::shared_ptr<PlayerUpdate> playerUpdate)
+{
+  SoundEngine();
+  m_Position.width = PLAYER_WIDTH;
+  m_Position.height = PLAYER_HEIGHT;
+  m_IsPaused = levelUpdate->getIsPausedPointer();
+}
+
+void PlayerUpdate::handleInput()
+{
+  m_InputReceiver.clearEvents();
+}
+
+void PlayerUpdate::update(float timeTakenThisFrame)
+{
+  handleInput();
+}
--- PlayerUpdate.h
+++ PlayerUpdate.h
@@ -3,7 +3,32 @@
 #include "InputReceiver.h"
 #include <SFML/Graphics.hpp>
 
-class PlayerUpdate {
+class PlayerUpdate : public Update {
+private:
+  const float PLAYER_WIDTH = 20.0f;
+  const float PLAYER_HEIGHT = 16.0f;
+  sf::FloatRect m_Position;
 
+  bool* m_IsPaused = nullptr;
+  float m_Gravity = 165;
+  float m_RunSpeed = 150;
+  float m_BoostSpeed = 250;
+  InputReceiver m_InputReceiver;
+  sf::Clock m_JumpClock;
+  bool m_SpaceHeldDown = false;
+  float m_JumpDuration = .5f;
+  float m_JumpSpeed = 400;
+public:
+  float m_RightIsHeldDown = false;
+  float m_LeftIsHeldDown = false;
+  float m_BoostIsHeldDown = false;
+  bool m_isGrounded;
+  bool m_InJump = false;
+  sf::FloatRect* getPositionPointer();
+  bool* getGroundedPointer();
+  void handleInput();
+  InputReceiver* getInputReceiver();
+  void assemble(std::shared_ptr<LevelUpdate> levelUpdate, std::shared_ptr<PlayerUpdate> playerUpdate) override;
+  void update(float fps) override;
 };
 
